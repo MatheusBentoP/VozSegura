2b3aa7b6b9ef7a862d24aff7d4435a23c73b163b
Matheus Bento <matheusbentopds@gmail.com>
2025-06-06 12:19:52 -0300

    first commit

---
+++ VozSegura-1/src/main/java/com/trabalho/VozSegura1/VozSeguraApplication.java
@@ -0,0 +1,13 @@
+package com.trabalho.VozSegura1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VozSeguraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VozSeguraApplication.class, args);
+	}
+
+}
